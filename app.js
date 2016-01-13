82e4e84fa427ad1db975ec745a859ce4fd569776
andres-root <andres.root.coder@gmail.com>
2016-01-12 18:46:50 -0800

    update

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 (function (){
+	var video = document.querySelector('#video');
+
+	video.addEventListener('click', function() {
+		var canvas = document.querySelector('#canvas');
+		var context = canvas.getContext('2d');
+
+		context.drawImage(video, 1, 1);
+	}, false);
+
 })();
